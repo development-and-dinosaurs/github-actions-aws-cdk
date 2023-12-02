8a74239cc4579529e259a97ac3a5bd6c578f2f45
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2023-12-02 15:47:34 +0000

    Update echo output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd "${INPUT_WORKING_DIRECTORY}" || exit
 echo "Working in $(pwd)"
-echo "${INPUT_COMMAND} \"${INPUT_STACKS}\""
+echo "cdk ${INPUT_COMMAND} \"${INPUT_STACKS}\""
 cdk "${INPUT_COMMAND} \"${INPUT_STACKS}\""
